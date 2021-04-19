0f0c3222c0c346cfdf36185f3342e08dfc7a382e
Genusuppal <harpranav2202@gmail.com>
2021-04-19 15:54:12 +0530

    7

--- javascript/index.js
+++ javascript/index.js
@@ -11,55 +11,80 @@ function changeImage() {
 
 
 function change(x) {
-    var image = document.getElementById('scroll');
     var a = "sm:px-6 py-3 w-1/2 sm:w-auto justify-center sm:justify-start border-b-2 title-font font-medium bg-gray-100 inline-flex items-center leading-none border-indigo-500 text-indigo-500 tracking-wider rounded-t";
     var b = "sm:px-6 py-3 w-1/2 sm:w-auto justify-center sm:justify-start border-b-2 title-font font-medium inline-flex items-center leading-none border-gray-200 hover:text-gray-900 tracking-wider";
+    var c = "menu-content-selected";
+    var d = "menu-content"
     var elemsSelected = document.getElementsByClassName(a);
     var elemSelected = elemsSelected.item(0);
-    if (x.match("about me")) {
-        var elemClicked = document.getElementById('menu about me');
+    var contentsSelected = document.getElementsByClassName(c);
+    var contentSelected = contentsSelected.item(0);
+    if (x.match("about-me")) {
+        var elemClicked = document.getElementById('menu ' + x);
+        var contentClicked = document.getElementById('menu-content ' + x);
         elemClicked.className = elemClicked.className.replace(b, a);
         elemSelected.className = elemSelected.className.replace(a, b);
+        contentClicked.className = contentClicked.className.replace(d, c);
+        contentSelected.className = contentSelected.className.replace(c, d);
         elemClicked.id = elemClicked.id.replace("menu", "menu-selected");
         elemSelected.id = elemSelected.id.replace("menu-selected", "menu");
-        image.src = "images//pic_bulbon.gif";
+        contentClicked.id = contentClicked.id.replace(d, c);
+        contentSelected.id = contentSelected.id.replace(c, d);
     } else if (x.match("resume")) {
-        var elemClicked = document.getElementById('menu resume');
+        var elemClicked = document.getElementById('menu ' + x);
+        var contentClicked = document.getElementById('menu-content ' + x);
         elemClicked.className = elemClicked.className.replace(b, a);
         elemSelected.className = elemSelected.className.replace(a, b);
+        contentClicked.className = contentClicked.className.replace(d, c);
+        contentSelected.className = contentSelected.className.replace(c, d);
         elemClicked.id = elemClicked.id.replace("menu", "menu-selected");
         elemSelected.id = elemSelected.id.replace("menu-selected", "menu");
-        //window.open('pages//resume.html');
-        loadWholePage('pages//resume.html');
-        image.src = "images//pic_bulboff.gif";
+        contentClicked.id = contentClicked.id.replace(d, c);
+        contentSelected.id = contentSelected.id.replace(c, d);
     } else if (x.match("contact")) {
-        var elemClicked = document.getElementById('menu contact');
+        var elemClicked = document.getElementById('menu ' + x);
+        var contentClicked = document.getElementById('menu-content ' + x);
         elemClicked.className = elemClicked.className.replace(b, a);
         elemSelected.className = elemSelected.className.replace(a, b);
+        contentClicked.className = contentClicked.className.replace(d, c);
+        contentSelected.className = contentSelected.className.replace(c, d);
         elemClicked.id = elemClicked.id.replace("menu", "menu-selected");
         elemSelected.id = elemSelected.id.replace("menu-selected", "menu");
-        image.src = "images//pic_bulboff.gif";
+        contentClicked.id = contentClicked.id.replace(d, c);
+        contentSelected.id = contentSelected.id.replace(c, d);
     } else if (x.match("blog")) {
-        var elemClicked = document.getElementById('menu blog');
+        var elemClicked = document.getElementById('menu ' + x);
+        var contentClicked = document.getElementById('menu-content ' + x);
         elemClicked.className = elemClicked.className.replace(b, a);
         elemSelected.className = elemSelected.className.replace(a, b);
+        contentClicked.className = contentClicked.className.replace(d, c);
+        contentSelected.className = contentSelected.className.replace(c, d);
         elemClicked.id = elemClicked.id.replace("menu", "menu-selected");
         elemSelected.id = elemSelected.id.replace("menu-selected", "menu");
-        image.src = "images//pic_bulboff.gif";
+        contentClicked.id = contentClicked.id.replace(d, c);
+        contentSelected.id = contentSelected.id.replace(c, d);
     } else if (x.match("projects")) {
-        var elemClicked = document.getElementById('menu projects');
+        var elemClicked = document.getElementById('menu ' + x);
+        var contentClicked = document.getElementById('menu-content ' + x);
         elemClicked.className = elemClicked.className.replace(b, a);
         elemSelected.className = elemSelected.className.replace(a, b);
+        contentClicked.className = contentClicked.className.replace(d, c);
+        contentSelected.className = contentSelected.className.replace(c, d);
         elemClicked.id = elemClicked.id.replace("menu", "menu-selected");
         elemSelected.id = elemSelected.id.replace("menu-selected", "menu");
-        image.src = "images//pic_bulboff.gif";
-    } else if (x.match("videos")) {
-        var elemClicked = document.getElementById('menu videos');
+        contentClicked.id = contentClicked.id.replace(d, c);
+        contentSelected.id = contentSelected.id.replace(c, d);
+    } else if (x.match("mylife")) {
+        var elemClicked = document.getElementById('menu ' + x);
+        var contentClicked = document.getElementById('menu-content ' + x);
         elemClicked.className = elemClicked.className.replace(b, a);
         elemSelected.className = elemSelected.className.replace(a, b);
+        contentClicked.className = contentClicked.className.replace(d, c);
+        contentSelected.className = contentSelected.className.replace(c, d);
         elemClicked.id = elemClicked.id.replace("menu", "menu-selected");
         elemSelected.id = elemSelected.id.replace("menu-selected", "menu");
-        image.src = "images//pic_bulboff.gif";
+        contentClicked.id = contentClicked.id.replace(d, c);
+        contentSelected.id = contentSelected.id.replace(c, d);
     }
 }
 
